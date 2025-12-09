5ef950dd9ae8f2aebe4461cb302b8a196cdb865e
1Hacker-anonimo <160126765+1Hacker-anonimo@users.noreply.github.com>
2025-12-09 12:49:12 -0300

    Update app.js

--- admin/js/app.js
+++ admin/js/app.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api';
+const API_URL = 'https://geladinho-digital-card-production.up.railway.app//api';
 let authToken = sessionStorage.getItem('authToken');
 let products = [];
 let sortableInstance = null;
